607eee466c8651fa8b000372b462892820a79f8b
JBSPC\jonat <jonathan.a.barnett@gmail.com>
2026-07-22 08:56:48 -0500

    Deploy web app to GitHub Pages

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,5 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base:
+    process.env.GITHUB_PAGES === 'true' ? '/High-Fantasy-Dice-Placement/' : '/',
   plugins: [react()],
 });
